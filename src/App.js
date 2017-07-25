4c708a1b3b5ec36e9b499cd074db5274839f13e3
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-07-25 05:36:35 +0100

    creates multiple input files. adds some focus buttons for testing ref

--- src/App.js
+++ src/App.js
@@ -3,16 +3,88 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputMessage: "",
+      displayMessage: "",
+      inputText:""
+    }
+  }
+
+  focus1=()=>{
+    this.textInput1.focus()
+  }
+  focus2=()=>{
+    this.textInput2.focus()
+  }
+
+  onInputChange = (e)=>{
+    this.setState({
+      inputMessage:e.target.value
+    })
+  }
+
+  handleKeyPress = (e)=>{
+    if(e.key==="Enter") {
+      this.sendMessage();
+    }
+  }
+
+  sendMessage = ()=>{
+    console.log("Message: ", this.state.inputMessage)
+    this.setState({
+      displayMessage: this.state.inputMessage
+    })
+  }
+
   render() {
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Input Fields Examples</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <br />
+          <h3>Type Something, and Hit Enter</h3>
+          <input
+            value={this.state.inputMessage}
+            onChange={this.onInputChange}
+            onKeyPress={this.handleKeyPress}
+          />
+        </div>
+        <h3>
+          {this.state.displayMessage}
+        </h3>
+
+        <div>
+          <br />
+          <h3>Multiple Input Fields and Enter</h3>
+          <br />
+          <button onClick={this.focus1}>Click me to focus1!</button>
+          <button onClick={this.focus2}>Click me to focus2!</button>
+          <br />
+          <input className="inputBox"
+            ref={(input)=>{ this.textInput1=input; }}
+          />
+          <input className="inputBox"
+            ref={(input)=>{ this.textInput2=input; }}
+          />
+          <input className="inputBox"
+            ref={(input)=>{ this.textInput3=input; }}
+          />
+          <input className="inputBox"
+            ref={(input)=>{ this.textInput4=input; }}
+          />
+          <input className="inputBox"
+            ref={(input)=>{ this.textInput5=input; }}
+          />
+        </div>
+
+
+
       </div>
     );
   }
